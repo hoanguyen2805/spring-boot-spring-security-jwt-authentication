2521d718a5b5ecc2c18905ad9be14890f7fe15cd
hoanguyen2805 <vanhoa280598@gmail.com>
2020-11-20 04:33:56 +0700

    first commit

---
+++ src/main/java/com/bezkoder/springjwt/payload/request/LoginRequest.java
@@ -0,0 +1,48 @@
+package com.bezkoder.springjwt.payload.request;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+	@NotBlank
+	private String username;
+
+	@NotBlank
+	private String password;
+	
+	private String phone;
+	
+	private String address;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
